af00c3fa5731eb8390c4a5e481db6f7bb4080e06
rimjhimart <rimjhim.gupta.ak@gmail.com>
2023-12-03 00:17:55 +0530

    first commit

---
+++ f1.js
@@ -0,0 +1,24 @@
+// variable type string
+let a = "rimu"
+b= 4;
+console.log(a + b)
+console.log(typeof (a+b))
+//ques 4  adding new key to canst object
+const c= {
+    name : "rimu",
+    section : 1
+}
+    c['friend'] = "alok"
+    c['section2'] = "5"
+    console.log(c)
+
+    //ques5  creating js for word dictionary
+    const dict = {
+        appreciate: "recognize the full worth of.",
+        ataraxia: "a state of freedom from emotional disturbance and anxiety",
+        yakka: "work, especially hard work."
+    }
+    console.log(dict['yakka'])
+
+
+
